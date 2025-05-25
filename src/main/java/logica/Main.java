60ba304e46992019a5f7aa3df81f2f5924e060c9
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-05-24 22:56:17 -0500

    Modified Main.java -added code to change the look and feel od the program

--- src/main/java/logica/Main.java
+++ src/main/java/logica/Main.java
@@ -1,19 +1,30 @@
 package logica;
 
+//<editor-fold defaultstate="collapsed" desc="Librerias del programa">
 import gui.Ventana;
+import logica.Tema;
+import javax.swing.SwingUtilities;//para cambair el tema del programa
+//</editor-fold>
 
 /**
  * Esta clase controla toda la lógica del programa
  * @author Emmanuelito18
  */
 public class Main {
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
-    public static void main(String[] args){
-        Ventana ventana=new Ventana();
-        ventana.setVisible(true);//Muestra la ventana del programa
-        ventana.setLocationRelativeTo(null);//Centra la ventana
+    public static void main(String[] args) {
+        Tema apariencia=new Tema();
+        apariencia.TemaOscuro();
+
+        // Crear la ventana en el hilo de Swing
+        SwingUtilities.invokeLater(() -> {
+            Ventana miVentana = new Ventana();// Crear la ventana
+            miVentana.setLocationRelativeTo(null);// Pone la ventana en el cenrto de la pantalla
+            miVentana.setVisible(true);// Mostrar la ventana
+        });
     }
 }
