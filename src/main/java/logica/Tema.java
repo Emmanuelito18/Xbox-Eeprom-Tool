f7111d12b74718dcbce6e372d07b4401fc35e003
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-05-27 00:53:25 -0500

    Modified dependencies -added code to change in real time the theme of the program

--- src/main/java/logica/Tema.java
+++ src/main/java/logica/Tema.java
@@ -6,18 +6,21 @@ import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 //</editor-fold>
 
 /**
  * Esta clase se encarga de controlar los temas para el programa principal
+ *
  * @author Emmanuelito18
  */
 public class Tema {
+
     /**
      * Cambia el tema del programa al tema Oscuro
      */
     //<editor-fold defaultstate="collapsed" desc="Tema oscuro">
-    public void TemaOscuro(){
+    public void TemaOscuro() {
         try {
             // Puedes usar Nimbus, FlatLaf u otro
             UIManager.setLookAndFeel(new FlatDarkLaf());
@@ -26,12 +29,12 @@ public class Tema {
         }
     }
     //</editor-fold>
-    
+
     /**
      * Cambia el tema del programa al tema Claro
      */
     //<editor-fold defaultstate="collapsed" desc="Tema claro">
-    public void TemaClaro(){
+    public void TemaClaro() {
         try {
             // Puedes usar Nimbus, FlatLaf u otro
             UIManager.setLookAndFeel(new FlatLightLaf());
@@ -39,5 +42,38 @@ public class Tema {
             e.printStackTrace();
         }
     }
+    //</editor-fold>
+
+    /**
+     * Actualiza el tema del programa en tiempo real al tema oscuro
+     *
+     * @param ventana La <code>ventana</code> en la que cambiará el tema
+     */
+    //<editor-fold defaultstate="collapsed" desc="Actualiza al tema oscuro">
+    public void actualizaTemaOscuro(JFrame ventana) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+            SwingUtilities.updateComponentTreeUI(ventana);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    //</editor-fold>
+    
+    /**
+     * Actualiza el tema del programa en tiempo real al tema claro
+     *
+     * @param ventana La <code>ventana</code> en la que cambiará el tema
+     */
+    //<editor-fold defaultstate="collapsed" desc="Actualiza al tema claro">
+    public void actualizarTemaClaro(JFrame ventana) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            SwingUtilities.updateComponentTreeUI(ventana);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println();
+    }
+    //</editor-fold>
 }
-//</editor-fold>
