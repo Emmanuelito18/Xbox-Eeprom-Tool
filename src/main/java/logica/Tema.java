4e0292dcee92153cefc25c44ff7a864a9e78fd1e
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-05-27 02:53:21 -0500

    Modified Tema.java -added code to change the theme of the program when the program starts

--- src/main/java/logica/Tema.java
+++ src/main/java/logica/Tema.java
@@ -2,6 +2,7 @@ package logica;
 
 // <editor-fold defaultstate="collapsed" desc="Librerias para look and feel">
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -20,7 +21,7 @@ public class Tema {
      * Cambia el tema del programa al tema Oscuro
      */
     //<editor-fold defaultstate="collapsed" desc="Tema oscuro">
-    public void TemaOscuro() {
+    public void temaOscuro() {
         try {
             // Puedes usar Nimbus, FlatLaf u otro
             UIManager.setLookAndFeel(new FlatDarkLaf());
@@ -34,7 +35,7 @@ public class Tema {
      * Cambia el tema del programa al tema Claro
      */
     //<editor-fold defaultstate="collapsed" desc="Tema claro">
-    public void TemaClaro() {
+    public void temaClaro() {
         try {
             // Puedes usar Nimbus, FlatLaf u otro
             UIManager.setLookAndFeel(new FlatLightLaf());
@@ -75,4 +76,26 @@ public class Tema {
     }
     //</editor-fold>
     
+    /**
+     * Aplica el tema por defecto la primera vez que se ejecuta el programa
+     * @param ventana <code>ventana</code> es la ventana en la que se aplicará el tema
+     */
+    //<editor-fold defaultstate="collapsed" desc="Aplica tema por defecto">
+    public void aplicarTemaPorDefecto(JFrame ventana){
+        boolean esOscuro = FlatLaf.isLafDark();
+        
+        try {
+            if (esOscuro) {
+                temaOscuro();
+            }else{
+                temaClaro();
+            }
+            if (ventana!=null) {
+                SwingUtilities.updateComponentTreeUI(ventana);
+            }
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+    //</editor-fold>
 }
