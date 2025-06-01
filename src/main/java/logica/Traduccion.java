fae0b142f351c0487aab7f262e3275eb988a0ecd
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-05-31 23:23:06 -0500

    Added Traduccion.java -created Traduccion.java

---
+++ src/main/java/logica/Traduccion.java
@@ -0,0 +1,10 @@
+package logica;
+
+/**
+ * Esta clase se encarga de hacer las traducciónes del programa en tiempo real
+ * utilizando archivos .properties para ello
+ * @author Emmanuelito18
+ */
+public class Traduccion {
+    
+}
