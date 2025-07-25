1dea7fe64333c9744a2a627df08115327b798c93
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-07-25 00:31:18 -0500

    Modified class PuertoCom.java to get com ports -added code to list COM ports

--- src/main/java/logica/PuertoCom.java
+++ src/main/java/logica/PuertoCom.java
@@ -1,6 +1,8 @@
 package logica;
 
 import com.fazecast.jSerialComm.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Esta clase se utliza para obtener todos los puertos COM disponibles
@@ -9,9 +11,23 @@ import com.fazecast.jSerialComm.*;
  */
 public class PuertoCom {
 
-    SerialPort[] puertos;
     public SerialPort[] obtenerPuertos() {
-        puertos = SerialPort.getCommPorts();
-        return puertos;
+        return SerialPort.getCommPorts();
+    }
+
+    public List<String> obtenerListaPuertos() {
+
+        List<String> lista = new ArrayList<>();
+        SerialPort[] puertos = obtenerPuertos();
+
+        if (puertos.length == 0) {
+            lista.add("No se encontraron puertos COM.");
+        } else {
+            for (SerialPort puerto : puertos) {
+                String descripcion = puerto.getSystemPortName() + " - " + puerto.getDescriptivePortName();
+                lista.add(descripcion);
+            }
+        }
+        return lista;
     }
 }
