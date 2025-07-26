cf7e87ba90aa5557d0a7123a044a523466294607
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-07-26 00:54:39 -0500

    Modify class EligeSistema.java to choose the correct command to run esptoll -added code to choose the correct commando to run esptool

--- src/main/java/logica/EligeSistema.java
+++ src/main/java/logica/EligeSistema.java
@@ -1,5 +1,7 @@
 package logica;
 
+import logica.InformacionSistema;
+
 /**
  * Esta clase se encarga de elegir la version de esptool correspondiente según
  * el sistema operativo y su arquitectura
@@ -8,4 +10,36 @@ package logica;
  */
 public class EligeSistema {
 
+    InformacionSistema informacion = new InformacionSistema();
+
+    public static void escogeSistema() {
+        String os = informacion.sistemaOperativo();
+        String arquitectura = informacion.arquitecturaSistema();
+
+        if (os.contains("win")) {
+            if (arquitectura.contains("64")) {
+                System.out.println("Windows 64 bits");
+            } else {
+                System.out.println("Windows 32 bits");
+            }
+        } else if (os.contains("mac")) {
+            if (arquitectura.contains("x64")) {
+                System.out.println("MacOS intel");
+            } else {
+                System.out.println("MacOS apple sillicon");
+            }
+        } else if (os.contains("nix") || os.contains("nux")) {
+            if (arquitectura.contains("64") || arquitectura.contains("amd64")) {
+                System.out.println("Linux 64 bits");
+            } else {
+                System.out.println("Linux 32 bits");
+            }
+
+            if (arquitectura.contains("aarch64")) {
+                System.out.println("Linux arm 64 bits");
+            } else {
+                System.out.println("Linux arm 32 bits");
+            }
+        }
+    }
 }
