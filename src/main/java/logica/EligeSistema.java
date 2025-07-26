3d54c7d2f2878716e2fb8d45d040599a60b6ed3d
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-07-25 22:07:54 -0500

    Add class EligeSistema.java to show notifications at the program -added class to choose the right command to run esptool for any os and architecture

---
+++ src/main/java/logica/EligeSistema.java
@@ -0,0 +1,11 @@
+package logica;
+
+/**
+ * Esta clase se encarga de elegir la version de esptool correspondiente según
+ * el sistema operativo y su arquitectura
+ *
+ * @author Emmanuelito18
+ */
+public class EligeSistema {
+
+}
