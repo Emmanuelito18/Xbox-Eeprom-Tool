8f62a97dd755d1422472fbcf8450eb7fbd4bbf98
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-08-06 17:06:04 -0500

    Modify class EjecutaEsptool.java to run esptool for any os and architecture -added methods to run esptool for each OS and architectures

--- src/main/java/logica/EjecutaEsptool.java
+++ src/main/java/logica/EjecutaEsptool.java
@@ -2,8 +2,64 @@ package logica;
 
 /**
  * Esta clase se utiliza para ejecutar cada una de las versiones de esptool
+ *
  * @author Emmanuelito18
  */
 public class EjecutaEsptool {
-    
+
+    /**
+     * Este método permite ejecutar esptool en Windows de 64 bits
+     */
+    public static void windows64() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en Windows de 32 bits
+     */
+    public static void windows32() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en MacOS con procesador intel
+     */
+    public static void macIntel() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en MacOS con procesador apple sillicon
+     */
+    public static void macApple() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en linux de 64 bits
+     */
+    public static void linux64() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en linux de 32 bits
+     */
+    public static void linux32() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en linux con arm64
+     */
+    public static void linuxArm64() {
+
+    }
+
+    /**
+     * Este método permite ejecutar esptool en linux con arm32
+     */
+    public static void linuxArm32() {
+
+    }
 }
