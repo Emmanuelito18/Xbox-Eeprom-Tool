81972e9b7f15bb9656502a52977a0cecd59bc50e
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-08-11 23:32:26 -0600

    Modify EjecutaEsptool.java -removed static modifier from all methods

--- src/main/java/logica/EjecutaEsptool.java
+++ src/main/java/logica/EjecutaEsptool.java
@@ -10,56 +10,56 @@ public class EjecutaEsptool {
     /**
      * Este método permite ejecutar esptool en Windows de 64 bits
      */
-    public static void windows64() {
-
+    public void windows64() {
+        System.out.println("Windows 64 bits");
     }
 
     /**
      * Este método permite ejecutar esptool en Windows de 32 bits
      */
-    public static void windows32() {
-
+    public void windows32() {
+        System.out.println("Windows 32 bits");
     }
 
     /**
      * Este método permite ejecutar esptool en MacOS con procesador intel
      */
-    public static void macIntel() {
-
+    public void macIntel() {
+        System.out.println("MacOS intel");
     }
 
     /**
      * Este método permite ejecutar esptool en MacOS con procesador apple sillicon
      */
-    public static void macApple() {
-
+    public void macApple() {
+        System.out.println("MacOS apple sillicon");
     }
 
     /**
      * Este método permite ejecutar esptool en linux de 64 bits
      */
-    public static void linux64() {
-
+    public void linux64() {
+        System.out.println("Linux 64 bits");
     }
 
     /**
      * Este método permite ejecutar esptool en linux de 32 bits
      */
-    public static void linux32() {
-
+    public void linux32() {
+        System.out.println("Linux 32 bits");
     }
 
     /**
      * Este método permite ejecutar esptool en linux con arm64
      */
-    public static void linuxArm64() {
-
+    public void linuxArm64() {
+        System.out.println("Linux arm 64 bits");
     }
 
     /**
      * Este método permite ejecutar esptool en linux con arm32
      */
-    public static void linuxArm32() {
-
+    public void linuxArm32() {
+        System.out.println("Linux arm 32 bits");
     }
 }
