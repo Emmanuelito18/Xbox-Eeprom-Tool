3831ccfbfa076c6c677ad562f8c85e0b964c6a99
Emmanuelito18 <emmanuelito18yt@gmail.com>
2026-03-27 00:26:52 -0600

    Add Arquitectura.java -added a class to define an enum for all architecture supported by esptool

---
+++ src/main/java/enums/Arquitectura.java
@@ -0,0 +1,13 @@
+package enums;
+
+/**
+ * Esta clase se en carga de definir el enum Arquitectura
+ * @author Emmanuelito18
+ */
+
+/**
+ * Enum para las arquitecturas de procesador soportadas por esptool
+ */
+public enum Arquitectura {
+    X86,X64,ARM32,ARM64,DESCONOCIDA
+}
