76fdf8b54534cc3da9581323cb1aec0c722fa55f
Emmanuelito18 <emmanuelito18yt@gmail.com>
2026-03-27 00:24:04 -0600

    Add SistemaOperativo.java -added a class to define an enum for all os supported by esptool

---
+++ src/main/java/enums/SistemaOperativo.java
@@ -0,0 +1,13 @@
+package enums;
+
+/**
+ * Esta clase se encarga de definir el enum SistemaOperativo
+ * @author Emmanuelito18
+ */
+
+/**
+ * Enum para los sistemas operativos soportados por esptool
+ */
+public enum SistemaOperativo {
+   WINDOWS,MAC,LINUX,DESCONOCIDO 
+}
