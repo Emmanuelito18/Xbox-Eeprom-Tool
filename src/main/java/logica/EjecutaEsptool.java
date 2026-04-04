9aa13ce459ac05bfa4ed6d38144d10ee509a998f
Emmanuelito18 <emmanuelito18yt@gmail.com>
2026-04-04 16:12:14 -0600

    Modify EjecutaEsptool.java -added IDE comments

--- src/main/java/logica/EjecutaEsptool.java
+++ src/main/java/logica/EjecutaEsptool.java
@@ -5,61 +5,79 @@ package logica;
  *
  * @author Emmanuelito18
  */
+// <editor-fold defaultstate="collapsed" desc="Clase EjecutaEsptool">
 public class EjecutaEsptool {
 
     /**
      * Este método permite ejecutar esptool en Windows de 64 bits
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Windows 64 bit">
     public void EsptoolWindows64() {
         System.out.println("Windows 64 bits");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en Windows de 32 bits
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Windows 32 bit">
     public void EsptoolWindows32() {
         System.out.println("Windows 32 bits");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en MacOS con procesador intel
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para MacOS intel">
     public void EsptoolMacIntel() {
         System.out.println("MacOS intel");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en MacOS con procesador apple sillicon
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para MacOS Apple Sillicon">
     public void EsptoolMacApple() {
         System.out.println("MacOS apple sillicon");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en linux de 64 bits
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Linux 64 bit">
     public void EsptoolLinux64() {
         System.out.println("Linux 64 bits");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en linux de 32 bits
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Linux 32 bit">
     public void EsptoolLinux32() {
         System.out.println("Linux 32 bits");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en linux con arm64
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Linux ARM 64 bit">
     public void EsptoolLinuxArm64() {
         System.out.println("Linux arm 64 bits");
     }
+    // </editor-fold>
 
     /**
      * Este método permite ejecutar esptool en linux con arm32
      */
+    // <editor-fold defaultstate="collapsed" desc="Esptool para Linux ARM 32 bit">
     public void EsptoolLinuxArm32() {
         System.out.println("Linux arm 32 bits");
     }
+    // </editor-fold>
 }
+// </editor-fold>
