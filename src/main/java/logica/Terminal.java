b920173ce75d216f145951cb107af164df84379a
Emmanuelito18 <emmanuelito18yt@gmail.com>
2026-04-05 13:56:58 -0600

    Add Terminal.java -added a class to execute several terminal commands from java

---
+++ src/main/java/logica/Terminal.java
@@ -0,0 +1,11 @@
+package logica;
+
+/**
+ * Esta clase se utiliza para ejecutar comandos de terminal
+ * @author Emmanuelito18
+ */
+// <editor-fold defaultstate="collapsed" desc="Clase Terminal">
+public class Terminal{
+    
+}
+// </editor-fold>
